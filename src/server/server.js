cefae35836bd2bd17f219e5eb7e2be6482be5886
Ying Zhang <ying.zhang_4@nxp.com>
2017-02-19 18:14:02 -0800

    push

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,35 @@ var react = require('react');
 var path = require('path');
 
 const server = express();
-server.use('/assets', express.static(path.join(__dirname, '..', '..', 'dist', 'assets'))) // express.static('assets'));
+server.use('/assets', express.static(path.join(__dirname, '..', '..', 'dist', 'assets'))); // express.static('assets'));
 
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'jade');
+
+// connect to sql server
+var sql = require("seriate");
+var config = {
+    "server": 'localhost',
+    "user": 'nxa27833',
+    "password": 'Happy@2016',
+    "database": 'webpage',
+    "domain": 'WBI',
+    "port": '49174'
+};
+sql.setDefaultConfig(config);
+
+// route
 server.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    sql.execute({
+        query: 'select TOP 10 * from vBOM_LOCATION_USER'
+    }).then( function(rows) {
+        console.log(rows);
+        res.render('index', {
+            rows: rows
+        });
+    }, function(err) {
+        console.log("Something bad happened: ", err);
+    });
 });
 
 server.listen(8080);
--- src/shared/components/table.jsx
+++ src/shared/components/table.jsx
@@ -1,28 +1,48 @@
 import Table from 'rc-table';
 const React = require('react');
-// const Table = require('rc-table').Table;
+
+// const columns = [
+//     {title: 'Name', dataIndex: 'name', key:'name', width: 100},
+//     {title: 'Age', dataIndex: 'age', key:'age', width: 100},
+//     {title: 'Address', dataIndex: 'address', key:'address', width: 200}
+//     // {title: 'Apeartions', dataIndex: '', key:'opeartions', render: () => <a href="#">Delete</a>}
+// ];
+
+// const data = [
+//     { name: 'Jack', age: 28, address: 'some where', key:'1' },
+//     { name: 'Rose', age: 36, address: 'some where', key:'2' }
+// ];
+
 
 const columns = [
-    {title: 'Name', dataIndex: 'name', key:'name', width: 100},
-    {title: 'Age', dataIndex: 'age', key:'age', width: 100},
-    {title: 'Address', dataIndex: 'address', key:'address', width: 200}
-    // {title: 'Apeartions', dataIndex: '', key:'opeartions', render: () => <a href="#">Delete</a>}
-];
-
-const data = [
-    { name: 'Jack', age: 28, address: 'some where', key:'1' },
-    { name: 'Rose', age: 36, address: 'some where', key:'2' }
+    {title: 'PEP', dataIndex: 'PEP', key:'PEP', width: 100},
+    {title: 'PEP Description', dataIndex: 'PEP Description', key:'PEP Description', width: 100},
+    {title: 'Backend Location', dataIndex: 'Backend Location', key:'Backend Location', width: 100},
+    {title: 'Component Level 1', dataIndex: 'Component Level 1', key:'Component Level 1', width: 100},
+    {title: 'Component Level 1 Desc.', dataIndex: 'Component Level 1 Desc.', key:'Component Level 1 Desc.', width: 100},
+    {title: 'Component Level 1 Category', dataIndex: 'Component Level 1 Category', key:'Component Level 1 Category', width: 100},
+    {title: 'Component Level 2', dataIndex: 'Component Level 2', key:'Component Level 2', width: 100},
+    {title: 'Component Level 2 Desc.', dataIndex: 'Component Level 2 Desc.', key:'Component Level 2 Desc.', width: 100},
+    {title: 'Component Level 2 Category', dataIndex: 'Component Level 2 Category', key:'Component Level 2 Category', width: 100},
+    {title: 'Component Level 2 Location', dataIndex: 'Component Level 2 Location', key:'Component Level 2 Location', width: 100},
+    {title: 'Component Level 3', dataIndex: 'Component Level 3', key:'Component Level 3', width: 100},
+    {title: 'Component Level 3 Desc.', dataIndex: 'Component Level 3 Desc.', key:'Component Level 3 Desc.', width: 100},
+    {title: 'Component Level 3 Category', dataIndex: 'Component Level 3 Category', key:'Component Level 3 Category', width: 100},
+    {title: 'Component Level 3 Location', dataIndex: 'Component Level 3 Location', key:'Component Level 3 Location', width: 100},
+    {title: 'Net Good Die', dataIndex: 'Net Good Die', key:'Net Good Die', width: 100},
 ];
 
 module.exports = React.createClass({
     render: function() {
+        console.log("In component!!!!!!!");
+        console.log(rows);
         return (
             <div>
                 <h1>Anxin Table</h1>
                 <Table
                     columns={columns}
                     scroll={{x:200, y:200}}
-                    data={data}
+                    data={rows}
                 />
             </div>
         );
