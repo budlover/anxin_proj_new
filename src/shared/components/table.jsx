327a1c9767f440fbf762af6fca8390c06c341c36
qiliu <qiliu@uber.com>
2017-02-20 20:09:56 -0800

    sdf

--- src/shared/components/table.jsx
+++ src/shared/components/table.jsx
@@ -38,7 +38,7 @@ module.exports = React.createClass({
         console.log(rows);
         return (
             <div>
-                <h1>Anxin Table</h1>
+                <h1>Demo Table</h1>
                 <Table
                     columns={columns}
                     scroll={{x:200, y:200}}
