45b8c765e45d6c00015d38b4ba4596877edb91c6
anxinpsu <zhangyingdalian@gmail.com>
2017-02-21 14:41:12 -0800

    Update table.jsx

--- src/shared/components/table.jsx
+++ src/shared/components/table.jsx
@@ -16,20 +16,20 @@ const React = require('react');
 
                 
 const columns = [
-    {title: 'PEP', dataIndex: 'PEP', key:'PEP'},
-    {title: 'PEP Desc.', dataIndex: 'PEP Description', key:'PEP Description'},
-    {title: 'BE Location', dataIndex: 'Backend Location', key:'Backend Location'},
-    {title: 'Component1', dataIndex: 'Component Level 1', key:'Component Level 1'},
-    {title: 'Component1 Desc.', dataIndex: 'Component Level 1 Desc.', key:'Component Level 1 Desc.'},
-    {title: 'Component1 Category', dataIndex: 'Component Level 1 Category', key:'Component Level 1 Category'},
-    {title: 'Component2', dataIndex: 'Component Level 2', key:'Component Level 2'},
-    {title: 'Component2 Desc.', dataIndex: 'Component Level 2 Desc.', key:'Component Level 2 Desc.'},
-    {title: 'Component2 Category', dataIndex: 'Component Level 2 Category', key:'Component Level 2 Category'},
-    {title: 'Component2 Location', dataIndex: 'Component Level 2 Location', key:'Component Level 2 Location'},
-    {title: 'Component3', dataIndex: 'Component Level 3', key:'Component Level 3'},
-    {title: 'Component3 Desc.', dataIndex: 'Component Level 3 Desc.', key:'Component Level 3 Desc.'},
-    {title: 'Component3 Category', dataIndex: 'Component Level 3 Category', key:'Component Level 3 Category'},
-    {title: 'Component3 Location', dataIndex: 'Component Level 3 Location', key:'Component Level 3 Location'},
+    {title: 'PEP', dataIndex: 'PEP', key:'PEP', width: 100},
+    {title: 'PEP Desc.', dataIndex: 'PEP Description', key:'PEP Description', width: 250},
+    {title: 'BE Location', dataIndex: 'Backend Location', key:'Backend Location', width: 100},
+    {title: 'Component1', dataIndex: 'Component Level 1', key:'Component Level 1', width: 100},
+    {title: 'Component1 Desc.', dataIndex: 'Component Level 1 Desc.', key:'Component Level 1 Desc.', width: 300},
+    {title: 'Component1 Category', dataIndex: 'Component Level 1 Category', key:'Component Level 1 Category', width: 100},
+    {title: 'Component2', dataIndex: 'Component Level 2', key:'Component Level 2', width:100},
+    {title: 'Component2 Desc.', dataIndex: 'Component Level 2 Desc.', key:'Component Level 2 Desc.', width: 100},
+    {title: 'Component2 Category', dataIndex: 'Component Level 2 Category', key:'Component Level 2 Category', width: 100},
+    {title: 'Component2 Location', dataIndex: 'Component Level 2 Location', key:'Component Level 2 Location', width:100},
+    {title: 'Component3', dataIndex: 'Component Level 3', key:'Component Level 3', width: 100},
+    {title: 'Component3 Desc.', dataIndex: 'Component Level 3 Desc.', key:'Component Level 3 Desc.', width: 100},
+    {title: 'Component3 Category', dataIndex: 'Component Level 3 Category', key:'Component Level 3 Category', width: 100},
+    {title: 'Component3 Location', dataIndex: 'Component Level 3 Location', key:'Component Level 3 Location', width:100},
     {title: 'Net Good Die', dataIndex: 'Net Good Die', key:'Net Good Die'},
 ];
 
@@ -39,11 +39,11 @@ module.exports = React.createClass({
         console.log(rows);
         return (
             <div>
-                <h1>Demo Table - PG Logic BOM</h1>
+                <h1>PG Logic BOM - Demo Table</h1>
                 <Table
                     useFixedHeader
                     columns={columns}
-                    scroll={{x:true, y:true}}
+                    scroll={{x:true, y:500}}
                     data={rows}
                  />
             </div>
