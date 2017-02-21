9f7d80d21d9aaaddd31081b485a7afe8e0b4ad65
anxinpsu <zhangyingdalian@gmail.com>
2017-02-21 13:45:23 -0800

    Update table.jsx

--- src/shared/components/table.jsx
+++ src/shared/components/table.jsx
@@ -21,21 +21,21 @@ const React = require('react');
 />
 
 const columns = [
-    {title: 'PEP', dataIndex: 'PEP', key:'PEP', width: 100},
-    {title: 'PEP Description', dataIndex: 'PEP Description', key:'PEP Description', width: 100},
-    {title: 'Backend Location', dataIndex: 'Backend Location', key:'Backend Location', width: 100},
-    {title: 'Component Level 1', dataIndex: 'Component Level 1', key:'Component Level 1', width: 100},
-    {title: 'Component Level 1 Desc.', dataIndex: 'Component Level 1 Desc.', key:'Component Level 1 Desc.', width: 100},
-    {title: 'Component Level 1 Category', dataIndex: 'Component Level 1 Category', key:'Component Level 1 Category', width: 100},
-    {title: 'Component Level 2', dataIndex: 'Component Level 2', key:'Component Level 2', width: 100},
-    {title: 'Component Level 2 Desc.', dataIndex: 'Component Level 2 Desc.', key:'Component Level 2 Desc.', width: 100},
-    {title: 'Component Level 2 Category', dataIndex: 'Component Level 2 Category', key:'Component Level 2 Category', width: 100},
-    {title: 'Component Level 2 Location', dataIndex: 'Component Level 2 Location', key:'Component Level 2 Location', width: 100},
-    {title: 'Component Level 3', dataIndex: 'Component Level 3', key:'Component Level 3', width: 100},
-    {title: 'Component Level 3 Desc.', dataIndex: 'Component Level 3 Desc.', key:'Component Level 3 Desc.', width: 100},
-    {title: 'Component Level 3 Category', dataIndex: 'Component Level 3 Category', key:'Component Level 3 Category', width: 100},
-    {title: 'Component Level 3 Location', dataIndex: 'Component Level 3 Location', key:'Component Level 3 Location', width: 100},
-    {title: 'Net Good Die', dataIndex: 'Net Good Die', key:'Net Good Die', width: 100},
+    {title: 'PEP', dataIndex: 'PEP', key:'PEP', y: 200, width: 200},
+    {title: 'PEP Description', dataIndex: 'PEP Description', key:'PEP Description', width: 200},
+    {title: 'Backend Location', dataIndex: 'Backend Location', key:'Backend Location', width: 200},
+    {title: 'Component Level 1', dataIndex: 'Component Level 1', key:'Component Level 1', width: 200},
+    {title: 'Component Level 1 Desc.', dataIndex: 'Component Level 1 Desc.', key:'Component Level 1 Desc.', width: 200},
+    {title: 'Component Level 1 Category', dataIndex: 'Component Level 1 Category', key:'Component Level 1 Category', width: 200},
+    {title: 'Component Level 2', dataIndex: 'Component Level 2', key:'Component Level 2', width: 200},
+    {title: 'Component Level 2 Desc.', dataIndex: 'Component Level 2 Desc.', key:'Component Level 2 Desc.', width: 200},
+    {title: 'Component Level 2 Category', dataIndex: 'Component Level 2 Category', key:'Component Level 2 Category', width: 200},
+    {title: 'Component Level 2 Location', dataIndex: 'Component Level 2 Location', key:'Component Level 2 Location', width: 200},
+    {title: 'Component Level 3', dataIndex: 'Component Level 3', key:'Component Level 3', width: 200},
+    {title: 'Component Level 3 Desc.', dataIndex: 'Component Level 3 Desc.', key:'Component Level 3 Desc.', width: 200},
+    {title: 'Component Level 3 Category', dataIndex: 'Component Level 3 Category', key:'Component Level 3 Category', width: 200},
+    {title: 'Component Level 3 Location', dataIndex: 'Component Level 3 Location', key:'Component Level 3 Location', width: 200},
+    {title: 'Net Good Die', dataIndex: 'Net Good Die', key:'Net Good Die', width: 200},
 ];
 
 module.exports = React.createClass({
@@ -44,7 +44,7 @@ module.exports = React.createClass({
         console.log(rows);
         return (
             <div>
-                <h1>Demo Table</h1>
+                <h1>Demo Table - PG Logic BOM</h1>
                 <Table
                     columns={columns}
                     scroll={{x:200, y:500}}
